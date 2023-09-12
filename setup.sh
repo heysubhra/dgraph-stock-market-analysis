66e02800af96a2a21a775d3f60dd895bcf5e110b
s <s@s.com>
2023-09-12 17:21:41 +0530

    Changed the GraphDB to Memgraph

--- setup.sh
+++ setup.sh
@@ -3,7 +3,9 @@ source .env
 set +a
 
 python3 -m venv .venv
-source .venv/bin/activate
+source ./.venv/bin/activate
 
 pip install -r requirements.txt
+source ~/.nvm/nvm.sh  
+npm install --global csv2json
 jupyter lab
